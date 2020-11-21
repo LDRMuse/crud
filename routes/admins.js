927c8f9a5131044e74abf6ed8122ff2421eafe36
Melissa <melissakheying@gmail.com>
2020-11-21 17:51:47 -0600

    :sparkles: create get and post requests for admins - tested in Postman

--- routes/admins.js
+++ routes/admins.js
@@ -1,9 +1,28 @@
+// all routes in here are starting with /admins
+
 import express from 'express'
 
 const router = express.Router();
 
-router.get('/admins', (req, res) => {
-  res.send('Hello from admins')
+const admins = [
+  {
+    firstName: "john",
+    lastName: "Doe",
+    age: "25"
+  }
+]
+
+router.get('/', (req, res) => {
+  res.send(admins)
+})
+
+router.post('/', (req, res) => {
+const admin = req.body;
+
+
+admins.push(admin)
+
+res.send(`Admin with the first name ${admin.firstName} has been created`)
 })
 
 export default router;
