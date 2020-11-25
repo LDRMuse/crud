0ac66f1fdf0734678f4a7adaabf46f17ae614ae1
Melissa <melissakheying@gmail.com>
2020-11-25 17:12:07 -0600

    :bug: fix cors issue

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ import session from 'express-session'
 import connectMongo from 'connect-mongo';
 import db from './db.js'
 import cors from 'cors'
-
 // allows us to take in incoming post request body
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv';
-
 // bring in the admins routes
 import adminsRoutes from './routes/admins.js'
 
@@ -25,18 +23,17 @@ let sessionOptions = session({
   cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
 })
 
-app.use(sessionOptions)
-app.use(cors())
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
+// We accept any client requests...but only CORS allowed for...
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
 app.use(express.json())
+app.use(sessionOptions)
 
 // tell express to use the admins routes
 // set the starting path for all the routes in the admins.js
 app.use('/admins', adminsRoutes);
 
-// route to the homepage
-app.get('/', (req, res) => res.send('Hello from home page'));
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`)
