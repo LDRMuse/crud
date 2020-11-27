0421cbaf37db4e07ac2f1aa97ada2309df0573af
Manav Misra <manavm@visionwebsoft.com>
2020-11-27 14:39:52 -0600

    :recycle: Replace 'explicit return' with 'implicit return'

--- client/src/components/views/Navbar.js
+++ client/src/components/views/Navbar.js
@@ -1,44 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const Navbar = () => {
-  return (
-    <div>
-      <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <button
-            className="navbar-burger burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </button>
+export const Navbar = () => (
+  <div>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <button
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </button>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <Link to="/" className="navbar-item">
+            Home
+          </Link>
         </div>
 
-        <div id="navbarBasicExample" className="navbar-menu">
-          <div className="navbar-start">
-            <Link to="/" className="navbar-item">
-              Home
-            </Link>
-          </div>
-
-          <div class="navbar-end">
-            <div class="navbar-item">
-              <div class="buttons">
-                <Link to="/admin/register" class="button is-primary">
-                  <strong>Want to be an Admin?</strong>
-                </Link>
-                <Link to="/admin/login" class="button is-light">
-                  Log in
-                </Link>
-              </div>
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <Link to="/admin/register" class="button is-primary">
+                <strong>Want to be an Admin?</strong>
+              </Link>
+              <Link to="/admin/login" class="button is-light">
+                Log in
+              </Link>
             </div>
           </div>
         </div>
-      </nav>
-    </div>
-  );
-};
+      </div>
+    </nav>
+  </div>
+);
