2dc2e819c429eef21c41c905f870dd35a2c37d11
Manav Misra <manavm@visionwebsoft.com>
2020-11-27 14:28:07 -0600

    Replace 'a' with 'button'

--- client/src/components/views/Navbar.js
+++ client/src/components/views/Navbar.js
@@ -1,43 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <div>
       <nav className="navbar" role="navigation" aria-label="main navigation">
-  <div className="navbar-brand">
-
-
-    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" className="navbar-menu">
-    <div className="navbar-start">
-      <Link to='/' className="navbar-item">
-        Home
-      </Link>
-
-
-    </div>
-
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <Link to='/admin/register' class="button is-primary">
-            <strong>Want to be an Admin?</strong>
-          </Link>
-          <Link to='/admin/login' class="button is-light">
-            Log in
-          </Link>
+        <div className="navbar-brand">
+          <button
+            className="navbar-burger burger"
+            aria-label="menu"
+            aria-expanded="false"
+            data-target="navbarBasicExample"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </button>
         </div>
-      </div>
+
+        <div id="navbarBasicExample" className="navbar-menu">
+          <div className="navbar-start">
+            <Link to="/" className="navbar-item">
+              Home
+            </Link>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <Link to="/admin/register" class="button is-primary">
+                  <strong>Want to be an Admin?</strong>
+                </Link>
+                <Link to="/admin/login" class="button is-light">
+                  Log in
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
-  </div>
-</nav>
-    </div>
-  )
-}
+  );
+};
