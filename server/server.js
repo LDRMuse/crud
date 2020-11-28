f46e1bf9dd01270e5ecc14820c8d45b7682655f4
Melissa <melissakheying@gmail.com>
2020-11-28 08:45:41 -0600

    :recycle: move .env & my server.js inside server folder
    renamed second server.js file to server2.js and commented out code for now.

--- server/server.js
+++ server/server.js
@@ -1,28 +1,40 @@
-import express from "express";
-import dotenv from "dotenv";
-
+import express from 'express';
+import session from 'express-session'
+import connectMongo from 'connect-mongo';
+import db from './db.js'
+import cors from 'cors'
+// allows us to take in incoming post request body
+import bodyParser from 'body-parser'
+import dotenv from 'dotenv';
 // bring in the admins routes
-import adminsRoutes from "./routes/admins.js";
+import adminsRoutes from './routes/admins.js'
 
 dotenv.config();
 
 const app = express();
-
-// ♻️ app.use(bodyParser.json());
-app.use(express.json());
-
-// ♻️ app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.urlencoded());
-
 const PORT = process.env.PORT;
+const MongoStore = connectMongo(session);
+
+let sessionOptions = session({
+  secret: process.env.SECRET,
+  store: new MongoStore({client: db}),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
+})
+
+// We accept any client requests...but only CORS allowed for...
+app.use(cors({
+  origin: 'http://localhost:3000',
+}));
+app.use(express.json())
+app.use(sessionOptions)
 
 // tell express to use the admins routes
 // set the starting path for all the routes in the admins.js
-app.use("/admins", adminsRoutes);
+app.use('/admins', adminsRoutes);
 
-// route to the homepage
-app.get("/", (req, res) => res.send("Hello from home page"));
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port: ${PORT}`);
-});
+  console.log(`Server is listening on port: ${PORT}`)
+})
---
+++ server/server2.js
@@ -0,0 +1,28 @@
+// import express from "express";
+// import dotenv from "dotenv";
+
+// // bring in the admins routes
+// import adminsRoutes from "./routes/admins.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// // ♻️ app.use(bodyParser.json());
+// app.use(express.json());
+
+// // ♻️ app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.urlencoded());
+
+// const PORT = process.env.PORT;
+
+// // tell express to use the admins routes
+// // set the starting path for all the routes in the admins.js
+// app.use("/admins", adminsRoutes);
+
+// // route to the homepage
+// app.get("/", (req, res) => res.send("Hello from home page"));
+
+// app.listen(PORT, () => {
+//   console.log(`Server is listening on port: ${PORT}`);
+// });
